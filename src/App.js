bcc717cae44f080dca1e4d77edb8a29987ed3521
Manishg <manscans16@gmail.com>
2023-01-04 13:13:58 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/Navbar"
 import './App.css';
+import { useContext } from "react";
+import { themeContext } from "./Context";
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}>
+     Manish Portfoloi
+     <Navbar />
     </div>
   );
 }
